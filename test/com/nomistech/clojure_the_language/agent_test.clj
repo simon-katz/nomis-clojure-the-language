cd3408e095ace8e4bfe0dfc770cda2dd4942c17e
Simon Katz <sk@nomistech.com>
2015-03-24 12:00:18 +0000

    Add com.nomistech.clojure-the-language.agent-test.

---
+++ test/com/nomistech/clojure_the_language/agent_test.clj
@@ -0,0 +1,26 @@
+(ns com.nomistech.clojure-the-language.agent-test
+  (:require [midje.sweet :refer :all]))
+
+(fact
+  (let [a (agent 0)]
+    @a)
+  => 0)
+
+(fact
+  (let [a (agent 0)]
+    (send a + 2)
+    (send a + 3)
+    (await a)
+    @a)
+  => 5)
+
+(defn non-negative [] (agent 0 :validator #(>= % 0)))
+
+(fact "On agents in a failed state, attempts to dispatch new actions fail."
+  (let [a (non-negative)]
+    (send a dec)
+    (Thread/sleep 100)
+    (try (send a dec)
+         (catch Exception _
+           :exception-thrown)))
+  => :exception-thrown)

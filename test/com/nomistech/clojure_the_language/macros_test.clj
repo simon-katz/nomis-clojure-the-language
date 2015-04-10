7b67e707b0218e88b91e06b9773691a092649bdd
Simon Katz <sk@nomistech.com>
2015-04-10 15:14:45 +0100

    More tests of macroexpansion.

--- test/com/nomistech/clojure_the_language/macros_test.clj
+++ test/com/nomistech/clojure_the_language/macros_test.clj
@@ -1,10 +1,11 @@
 (ns com.nomistech.clojure-the-language.macros-test
   (:require [midje.sweet :refer :all]))
 
-(defmacro goo [x] `(list a x ~x))
+(defmacro goo [x] `(list x ~'x ~x '~x))
 
 (fact
-  (macroexpand-1 '(goo plop))
-  => '(clojure.core/list com.nomistech.clojure-the-language.macros-test/a
-                         com.nomistech.clojure-the-language.macros-test/x
-                         plop))
+  (macroexpand-1 '(goo x-in-call))
+  => '(clojure.core/list com.nomistech.clojure-the-language.macros-test/x
+                         x
+                         x-in-call
+                         (quote x-in-call)))

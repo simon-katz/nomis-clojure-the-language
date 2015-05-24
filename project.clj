1154280cb5729bd3fb4024a705d594670a9b9952
Simon Katz <sk@nomistech.com>
2015-05-24 10:12:07 +0100

    Remove unwanted dependency on tools.namespace. (Have that in user profile.)

--- project.clj
+++ project.clj
@@ -4,5 +4,4 @@
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
   :profiles
   {:dev {:dependencies [[midje "1.6.3"]]
-         :plugins [[lein-midje "3.1.3"]
-                   [org.clojure/tools.namespace "0.2.5"]]}})
+         :plugins [[lein-midje "3.1.3"]]}})

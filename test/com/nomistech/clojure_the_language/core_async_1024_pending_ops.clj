063e896d4d4b3d0a9bc4cdcb59629ad2cd52bbab
Simon Katz <sk@nomistech.com>
2015-05-24 13:22:20 +0100

    core.async: Add stuff about no more than 1024 operations.

---
+++ test/com/nomistech/clojure_the_language/core_async_1024_pending_ops.clj
@@ -0,0 +1,16 @@
+(ns com.nomistech.clojure-the-language.core-async-1024-pending-ops
+  (:require
+   ;; [com.nomistech.clojure-the-language.core-async-1024-pending-ops :refer :all]
+   [clojure.core.async :as a
+    :exclude [map into reduce merge partition partition-by take]]
+   [midje.sweet :refer :all]))
+
+(fact "About pending operations"
+  
+  (fact "No more than 1024 pending puts"
+    (let [c (a/chan)] (dotimes [i 1025] (a/put! c i)))
+    => (throws AssertionError #"No more than 1024 pending puts"))
+  
+  (fact "No more than 1024 pending takes"
+    (let [c (a/chan)] (dotimes [i 1025] (a/take! c #(do 42))))
+    => (throws AssertionError #"No more than 1024 pending takes")))

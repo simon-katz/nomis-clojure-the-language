3b840404cb9eb83a5b17c5c97ab21bf27963d815
Simon Katz <sk@nomistech.com>
2015-05-24 09:09:58 +0100

    Add core.match 101. (Moved from another place.)

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject com.nomistech/clojure-the-language "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.match "0.3.0-alpha4"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
   :profiles
   {:dev {:dependencies [[midje "1.6.3"]]

b12856b29ff1a530f93c95cc64be00b95774e6ec
Simon Katz <sk@nomistech.com>
2015-10-17 17:02:38 +0100

    Macros: Rename namespace alias.

--- test/com/nomistech/clojure_the_language/macros/syms_test.clj
+++ test/com/nomistech/clojure_the_language/macros/syms_test.clj
@@ -1,11 +1,11 @@
 (ns com.nomistech.clojure-the-language.macros.syms-test
-  (:require [com.nomistech.clojure-the-language.macros.syms :as defs]
+  (:require [com.nomistech.clojure-the-language.macros.syms :as syms]
             [midje.sweet :refer :all]))
 
 ;;;; ___________________________________________________________________________
 
 (fact
-  (macroexpand-1 '(defs/do-things-with-symbols-1 x-in-call))
+  (macroexpand-1 '(syms/do-things-with-symbols-1 x-in-call))
   => '{:unquote        x-in-call
        :quote-unquote 'x-in-call
        :force-capture  x-in-def
@@ -13,7 +13,7 @@
        :quote         'com.nomistech.clojure-the-language.macros.syms/x-in-def})
 
 (fact
-  (macroexpand-1 '(defs/do-things-with-symbols-2))
+  (macroexpand-1 '(syms/do-things-with-symbols-2))
   => '{:undecorated-value-symbol   clojure.core/*print-level*
        :undecorated-fun-symbol     clojure.core/+
        :undecorated-macro-symbol   clojure.core/cond

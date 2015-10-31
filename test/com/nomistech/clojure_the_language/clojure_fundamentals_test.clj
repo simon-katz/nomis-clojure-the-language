5675a137092fd98f4b273b606117fdce2345f865
Simon Katz <sk@nomistech.com>
2015-10-31 15:55:26 +0000

    Move demo of chunked sequences to separate file.

--- test/com/nomistech/clojure_the_language/clojure_fundamentals_test.clj
+++ test/com/nomistech/clojure_the_language/clojure_fundamentals_test.clj
@@ -210,64 +210,6 @@
            java.lang.IndexOutOfBoundsException))
 
 ;;;; ___________________________________________________________________________
-;;;; Chunked sequences
-
-;;;; See http://blog.fogus.me/2010/01/22/de-chunkifying-sequences-in-clojure/
-;;;; (the content of which is included in The Joy of Clojure):
-
-(defn demo-chunked
-  "Take the first item from a chunked lazy sequence of size n, and
-  print a dot for each item in the list that is realized."
-  [n]
-  (letfn [(identity-with-print-dot [x]
-            (print \.)
-            x)]
-    (take 1 (map identity-with-print-dot
-                 (range n)))))
-
-(fact "Demo of chunked sequences -- in chunks of size 32"
-  (do (with-out-str (doall (demo-chunked 1)))
-      => ".")
-  (do (with-out-str (doall (demo-chunked 2)))
-      => "..")
-  (do (with-out-str (doall (demo-chunked 31)))
-      => "...............................")
-  (do (with-out-str (doall (demo-chunked 32)))
-      => "................................")
-  (do (with-out-str (doall (demo-chunked 33)))
-      => "................................")
-  (do (with-out-str (doall (demo-chunked 34)))
-      => "................................")
-  (do (with-out-str (doall (demo-chunked 1000)))
-      => "................................"))
-
-;;; Built-in functions use chunk-buffer and chunk-cons:
-;;;
- ;; (defn range
- ;;   "Returns a lazy seq of nums from start (inclusive) to end
- ;;   (exclusive), by step, where start defaults to 0, step to 1, and end
- ;;   to infinity."
- ;;   {:added "1.0"
- ;;    :static true}
- ;;   ([] (range 0 Double/POSITIVE_INFINITY 1))
- ;;   ([end] (range 0 end 1))
- ;;   ([start end] (range start end 1))
- ;;   ([start end step]
- ;;    (lazy-seq
- ;;     (let [b (chunk-buffer 32)
- ;;           comp (if (pos? step) < >)]
- ;;       (loop [i start]
- ;;         (if (and (< (count b) 32)
- ;;                  (comp i end))
- ;;           (do
- ;;             (chunk-append b i)
- ;;             (recur (+ i step)))
- ;;           (chunk-cons (chunk b)
- ;;                       (when (comp i end)
- ;;                         (range i end step)))))))))
-
-
-;;;; ___________________________________________________________________________
 ;;;; %& in #(...) functions (what are these called?)
 
 ;;;; In 4 Clojure Dot Product, I did this...

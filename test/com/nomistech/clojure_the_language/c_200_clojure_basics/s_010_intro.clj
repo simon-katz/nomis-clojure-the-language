f1c1d31d9239d99d1932b8fd88a891484c572349
Simon Katz <sk@nomistech.com>
2015-11-01 20:38:28 +0000

    Add (empty) files for Clojure Basics.

---
+++ test/com/nomistech/clojure_the_language/c_200_clojure_basics/s_010_intro.clj
@@ -0,0 +1,2 @@
+(ns com.nomistech.clojure-the-language.c-200-clojure-basics.s-010-intro
+  (:require [midje.sweet :refer :all]))
---
+++ test/com/nomistech/clojure_the_language/c_200_clojure_basics/s_210_intro_to_clojure_data_structures.clj
@@ -0,0 +1,2 @@
+(ns com.nomistech.clojure-the-language.c-200-clojure-basics.s-210-intro-to-clojure-data-structures
+  (:require [midje.sweet :refer :all]))

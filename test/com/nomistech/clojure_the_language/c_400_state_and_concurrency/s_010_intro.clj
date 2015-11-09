1e58f1066c4c75cf931c390bedaa08c6e34e2c3b
Simon Katz <sk@nomistech.com>
2015-11-09 10:59:56 +0000

    Notes on state.

--- test/com/nomistech/clojure_the_language/c_400_state_and_concurrency/s_010_intro.clj
+++ test/com/nomistech/clojure_the_language/c_400_state_and_concurrency/s_010_intro.clj
@@ -2,12 +2,28 @@
   (:require [midje.sweet :refer :all]))
 
 ;;;; ___________________________________________________________________________
-;;;; State management and concurrency and without locks.
-;;;;
-;;;; References
-;;;; - There are a fair few types of reference.
-;;;;   - atoms, refs, vars, agents, promises, futures...
-;;;;     - confusing names! A ref is one kind of reference.
-;;;; - Some are primarily for managing state.
-;;;; - Some are primarily for managing concurrency.
-;;;; - Some combine the two.
+;;;; State management and concurrency and without locks
+
+;;;; Clojure is an impure functional language
+;;;; - It supports side effects
+
+;;;; Clojure has an approach to managing state
+;;;; - and sane concurrency
+
+;;;; Reference types
+;;;; - Each reference type provides a way of doing stateful and/or concurrent
+;;;;   things
+;;;; - Some are primarily for managing state
+;;;; - Some are primarily for managing concurrency
+;;;; - Some combine the two
+;;;; - Without using locks
+
+;;;; Kinds of reference type
+;;;; - atoms
+;;;; - refs (confusing -- a ref is a kind of reference)
+;;;; - vars
+;;;; - agents
+;;;; - futures
+;;;; - delays
+;;;; - promises
+

539c911edbbb2ed6c888a960a70891902d8d24f6
Simon Katz <sk@nomistech.com>
2015-11-10 21:56:58 +0000

    State.

--- test/com/nomistech/clojure_the_language/c_400_state_and_concurrency/s_030_refs.clj
+++ test/com/nomistech/clojure_the_language/c_400_state_and_concurrency/s_030_refs.clj
@@ -16,16 +16,16 @@
 ;;;; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ;;;; `ref`, `dosync`, `alter`
 
-(def bank-account-1-atom (ref 100))
-(def bank-account-2-atom (ref 200))
+(def bank-account-1-ref (ref 100))
+(def bank-account-2-ref (ref 200))
 
 (fact
   (let [amount 20]
     (dosync
-     (alter bank-account-1-atom + amount)
-     (alter bank-account-2-atom - amount))
-    [@bank-account-1-atom
-     @bank-account-2-atom])
+     (alter bank-account-1-ref + amount)
+     (alter bank-account-2-ref - amount))
+    [@bank-account-1-ref
+     @bank-account-2-ref])
   => [120
       180])
 

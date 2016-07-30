f0ee8cf6bd09494ffe93610b8d2e23c99c39c1b7
Simon Katz <sk@nomistech.com>
2016-07-30 13:25:08 +0100

    More depth on macros that use private things

--- test/com/nomistech/clojure_the_language/c_700_macros/s_920a_macro_using_private.clj
+++ test/com/nomistech/clojure_the_language/c_700_macros/s_920a_macro_using_private.clj
@@ -1,13 +1,50 @@
 (ns com.nomistech.clojure-the-language.c-700-macros.s-920a-macro-using-private)
 
-(defmacro public-fun []
-  1)
+;;;; ___________________________________________________________________________
 
-(defmacro macro-whose-expansion-is-a-call-of-a-public-thing []
+(defn public-fun []
+  :public-fun)
+
+(defmacro expand-to-public-fun []
   `(public-fun))
 
-(defmacro ^:private private-fun []
-  2)
+;;;; ___________________________________________________________________________
 
-(defmacro macro-whose-expansion-is-a-call-of-a-private-thing []
+(defmacro public-macro []
+  :public-macro)
+
+(defmacro expand-to-public-macro []
+  `(public-macro))
+
+;;;; ___________________________________________________________________________
+
+(defn ^:private private-fun []
+  :private-fun)
+
+(defmacro expand-to-private-fun []
   `(private-fun))
+
+;;;; ___________________________________________________________________________
+
+(defmacro ^:private private-macro []
+  :private-macro)
+
+(defmacro expand-to-private-macro []
+  `(private-macro))
+
+;;;; ___________________________________________________________________________
+
+(defn ^:private private-fun-with-workaround []
+  :private-fun-with-workaround)
+
+(defmacro expand-to-private-fun-with-workaround []
+  `(#'private-fun-with-workaround))
+
+;;;; ___________________________________________________________________________
+
+(defmacro ^:private private-macro-attempted-workaround []
+  :private-macro-attempted-workaround)
+
+(defmacro expand-to-private-macro-attempted-workaround []
+  `(#'private-macro-attempted-workaround) ; Not a good idea -- this will get the underlying function, not the macro
+  )

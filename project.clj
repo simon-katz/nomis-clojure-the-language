cbe60162e4bd8730492ce52dd325411efc9e4ce4
Simon Katz <sk@nomistech.com>
2016-10-30 09:28:28 +0000

    Move tailing stuff to nomis-tailer

--- project.clj
+++ project.clj
@@ -3,8 +3,7 @@
                  [cheshire "5.4.0"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [prismatic/schema "1.0.5"]
-                 [commons-io/commons-io "2.5"]]
+                 [prismatic/schema "1.0.5"]]
   :repl-options {:init-ns user}
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]
                                   [midje "1.7.0"]]

91d7d114bd7d7b6c520e54df95e52e58d7ab86b3
Simon Katz <sk@nomistech.com>
2017-05-14 13:32:15 +0100

    Add `chan->seq`

--- test/com/nomistech/clojure_the_language/c_850_utils/s_200_test_utils.clj
+++ test/com/nomistech/clojure_the_language/c_850_utils/s_200_test_utils.clj
@@ -1,5 +1,6 @@
 (ns com.nomistech.clojure-the-language.c-850-utils.s-200-test-utils
-  (:require [midje.sweet :refer :all]))
+  (:require [clojure.core.async :as a]
+            [midje.sweet :refer :all]))
 
 ;;;; ___________________________________________________________________________
 
@@ -7,3 +8,30 @@
   (clojure.pprint/pprint
    (clojure.string/split (System/getProperty "java.class.path")
                          #":")))
+
+;;;; ___________________________________________________________________________
+
+(defn chan->seq [c]
+  (lazy-seq
+   (when-let [v (a/<!! c)]
+     (cons v (chan->seq c)))))
+
+(fact "`chan->seq` works"
+  
+  (fact "can take elements before the channel closes"
+    (let [c (a/chan 10)]
+      (a/go (a/>! c 1)
+            (a/>! c 2)
+            (a/>! c 3))
+      (->> (chan->seq c)
+           (take 3)))
+    => [1 2 3])
+  
+  (fact "with a closed channel, the sequence ends"
+    (let [c (a/chan 10)]
+      (a/go (a/>! c 1)
+            (a/>! c 2)
+            (a/>! c 3)
+            (a/close! c))
+      (chan->seq c))
+    => [1 2 3]))

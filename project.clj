2ab5111d0fa775a6b05e84da833d2abb3764bb7f
Simon Katz <sk@nomistech.com>
2017-05-19 18:19:28 +0100

    Upgrade to Clojure 1.8

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject com.nomistech/clojure-the-language "0.1.0-SNAPSHOT"
   :dependencies [[cheshire "5.4.0"]
                  [com.taoensso/timbre "4.8.0"]
-                 [org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.3.442"] 
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [prismatic/schema "1.0.5"]]

dcb1f0995580daae10e6e84fbb511bd1a8b67da4
Simon Katz <sk@nomistech.com>
2017-05-21 10:35:05 +0100

    Remove commented-out stuff from `user` ns form

--- dev/user.clj
+++ dev/user.clj
@@ -2,8 +2,7 @@
   "Namespace to support hacking at the REPL."
   (:require [clojure.java.javadoc :refer [javadoc]]
             [clojure.pprint :refer [pp pprint]]
-            [clojure.repl :refer :all ; [apropos dir doc find-doc pst source]
-             ]
+            [clojure.repl :refer :all]
             [clojure.string :as str]
             [clojure.tools.namespace.move :refer :all]
             [clojure.tools.namespace.repl :refer :all]

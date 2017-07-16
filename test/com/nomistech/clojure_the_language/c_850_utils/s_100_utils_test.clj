e0f527bc9ffef36742db1b1f55de9fc412fb7cfb
Simon Katz <sk@nomistech.com>
2017-07-16 20:21:25 +0100

    Add `invert-injection` and `invert-non-injection` to utils

--- test/com/nomistech/clojure_the_language/c_850_utils/s_100_utils_test.clj
+++ test/com/nomistech/clojure_the_language/c_850_utils/s_100_utils_test.clj
@@ -121,6 +121,31 @@
       :b 3})
 
 ;;;; ___________________________________________________________________________
+;;;; ---- invert-injection invert-non-injection ----
+
+(fact "`invert-injection` works"
+  (invert-injection {:a 1
+                     :b 2
+                     :c 3
+                     :d 1}
+                    [:a :b :c :d :e])
+  =>
+  {1 [:a :d]
+   2 [:b]
+   3 [:c]})
+
+(fact "`invert-non-injection` works"
+  (invert-non-injection {:a [1 2]
+                         :b [2 3]
+                         :c []
+                         :d [2]}
+                        [:a :b :c :d :e])
+  =>
+  {1 [:a]
+   2 [:a :b :d]
+   3 [:b]})
+
+;;;; ___________________________________________________________________________
 ;;;; ---- with-extras ----
 
 (fact "`with-extras` works"

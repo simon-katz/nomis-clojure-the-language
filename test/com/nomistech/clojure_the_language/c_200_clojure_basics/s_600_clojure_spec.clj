65b02e54d1e62b2d7d4a2a15151d80e1b1c41163
Simon Katz <sk@nomistech.com>
2018-01-27 12:21:50 +0000

    clojure.spec: Add intro to `valid?`

--- test/com/nomistech/clojure_the_language/c_200_clojure_basics/s_600_clojure_spec.clj
+++ test/com/nomistech/clojure_the_language/c_200_clojure_basics/s_600_clojure_spec.clj
@@ -5,10 +5,10 @@
 ;;;; ___________________________________________________________________________
 ;;;; Stuff from https://clojure.org/guides/spec
 
-(fact
-  (s/conform even? 1000)
-  => 1000)
+(fact "Intro to `conform`: spec X value -> value"
+  (fact (s/conform even? 1000) => 1000)
+  (fact (s/conform even? 1001) => :clojure.spec.alpha/invalid))
 
-(fact
-  (s/conform even? 999)
-  => :clojure.spec.alpha/invalid)
+(fact "Intro to `valid?`: spec X value -> boolean"
+  (fact (s/valid? even? 1000) => true)
+  (fact (s/valid? even? 1001) => false))

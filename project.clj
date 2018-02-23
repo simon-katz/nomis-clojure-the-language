6cac12e9ce4516f7eece9c323dd200107bc5cc50
Simon Katz <sk@nomistech.com>
2018-02-23 14:47:54 +0000

    Upgrade some dependencies

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject com.nomistech/clojure-the-language "0.1.0-SNAPSHOT"
-  :dependencies [[cheshire "5.4.0"]
+  :dependencies [[cheshire "5.8.0"]
                  [com.climate/claypoole "1.1.4"]
-                 [com.taoensso/timbre "4.8.0"]
+                 [com.taoensso/timbre "4.10.0"]
                  [org.clojure/clojure "1.9.0"]
-                 [org.clojure/core.async "0.3.442"] 
-                 [org.clojure/core.match "0.3.0-alpha4"]
-                 [prismatic/schema "1.0.5"]
+                 [org.clojure/core.async "0.4.474"] 
+                 [org.clojure/core.match "0.3.0-alpha5"]
+                 [prismatic/schema "1.1.7"]
                  [slingshot "0.12.2"]]
   :repl-options {:init-ns user}
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]

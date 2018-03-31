c4c87915ea9055eaa3c436e9eca66765192c30a0
Simon Katz <sk@nomistech.com>
2018-03-31 19:08:56 +0100

    Add `bidi` and `yada` dependencies

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject com.nomistech/clojure-the-language "0.1.0-SNAPSHOT"
-  :dependencies [[cheshire "5.8.0"]
+  :dependencies [[bidi "2.1.3" :exclusions [ring/ring-core]]
+                 [cheshire "5.8.0"]
                  [com.climate/claypoole "1.1.4"]
                  [com.taoensso/timbre "4.10.0" :exclusions [io.aviso/pretty
                                                             org.clojure/tools.reader]]
@@ -7,10 +8,11 @@
                  [org.clojure/core.async "0.4.474"] 
                  [org.clojure/core.match "0.3.0-alpha5"]
                  [prismatic/schema "1.1.7"]
-                 [ring/ring-json "0.4.0"]
-                 [slingshot "0.12.2"]]
+                 [ring/ring-json "0.4.0" :exclusions [ring/ring-core]]
+                 [slingshot "0.12.2"]
+                 [yada "1.2.11"]]
   :repl-options {:init-ns user}
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]
-                                  [midje "1.9.1"]]
+                                  [midje "1.9.1" :exclusions [riddley]]]
                    :source-paths ["dev"]
                    :plugins [[lein-midje "3.2.1"]]}})

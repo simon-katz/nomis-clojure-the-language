a843949a4e289f6e9ea12a6630dcb7aa4d1b6591
Simon Katz <simon.katz@youview.com>
2018-10-18 13:48:11 +0100

    Improve `for-all` examples

--- test/com/nomistech/clojure_the_language/c_800_libraries/s_105_generative_testing/midje_gen_test.clj
+++ test/com/nomistech/clojure_the_language/c_800_libraries/s_105_generative_testing/midje_gen_test.clj
@@ -3,28 +3,30 @@
             [midje.experimental :refer [for-all]]
             [midje.sweet :refer :all]))
 
-(for-all
- ;; 'the-name
- "A doc string"
- [positive-num gen/s-pos-int
-  int          gen/int]
- {:max-size 10
-  :num-tests 15
-  :seed 1510160943861}
- (fact "An integer added to a positive number is always a number?"
-   (+ positive-num int) => integer?)
- #_(fact "An integer added to a positive number is always positive?"
-   (+ positive-num int) => pos?))
+;;;; ___________________________________________________________________________
+;;;; From https://github.com/marick/Midje/wiki/Generative-testing-with-for-all
+
+(for-all "Midge generative test example #1 from Midje Wiki page"
+    [positive-num gen/s-pos-int
+     int          gen/int]
+  {:max-size 10
+   :num-tests 15
+   :seed 1510160943861}
+  (fact "An integer added to a positive number is always a number?"
+    (+ positive-num int) => integer?)
+  #_(fact "An integer added to a positive number is always positive?"
+      (+ positive-num int) => pos?))
 
 (defn my-vals [a-map] (map second a-map))
 
-(for-all
- [str-map (gen/map gen/keyword gen/string)]
- {:max-size 10
-  :num-tests 15
-  :seed 1510160943861}
- (fact "extracted keys are strings"
-   (my-vals str-map) => (has every? string?))
- (when-not (empty? str-map)
-   (fact "my-vals matches keys behavior"
-     (my-vals str-map) => (vals str-map))))
+(for-all "Midge generative test example #2 from Midje Wiki page"
+    [str-map (gen/map gen/keyword gen/string)]
+  {:max-size 10
+   :num-tests 15
+   :seed 1510160943861}
+  (fact "extracted keys are strings"
+    (my-vals str-map) => (has every? string?))
+  (when-not (empty? str-map)
+    (fact "my-vals matches keys behavior"
+      (my-vals str-map) => (vals str-map))))
+

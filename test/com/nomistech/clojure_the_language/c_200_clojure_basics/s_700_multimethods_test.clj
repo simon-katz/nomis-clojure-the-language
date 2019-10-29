2ccfc7164ad7ba70201aa20b57149add9f3a3dbd
Simon Katz <sk@nomistech.com>
2019-10-29 15:54:48 +0000

    Add examples of multimethods

---
+++ test/com/nomistech/clojure_the_language/c_200_clojure_basics/s_700_multimethods_test.clj
@@ -0,0 +1,14 @@
+(ns com.nomistech.clojure-the-language.c-200-clojure-basics.s-700-multimethods-test
+  (:require [midje.sweet :refer :all]))
+
+(defmulti multi-thing :kind)
+
+(defmethod multi-thing :a [_] 1)
+(defmethod multi-thing :b [_] 2)
+(defmethod multi-thing :c [_] 3)
+
+(fact "About `multi-thing`"
+  (multi-thing {}) => (throws #"No method in multimethod")
+  (multi-thing {:kind :a}) => 1
+  (multi-thing {:kind :b}) => 2
+  (multi-thing {:kind :c}) => 3)

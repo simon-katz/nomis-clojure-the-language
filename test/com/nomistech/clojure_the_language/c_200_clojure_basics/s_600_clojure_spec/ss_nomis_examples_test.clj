e88f5f182997d0e49c9db71ba151b390366466f6
Simon Katz <sk@nomistech.com>
2019-11-14 17:32:52 +0000

    Add clojure.spec examples

---
+++ test/com/nomistech/clojure_the_language/c_200_clojure_basics/s_600_clojure_spec/ss_nomis_examples_test.clj
@@ -0,0 +1,54 @@
+(ns com.nomistech.clojure-the-language.c-200-clojure-basics.s-600-clojure-spec.ss-nomis-examples-test
+  (:require [clojure.spec.alpha :as s]
+            [midje.sweet :refer :all]))
+
+;;;; ___________________________________________________________________________
+;;;; Use `s/spec` to wrap things for nesting.
+
+;;;; Example from https://clojure.org/guides/spec#_sequences
+
+(s/def ::nested
+  (s/cat :names-kw #{:names}
+         :names (s/spec (s/* string?))
+         :nums-kw #{:nums}
+         :nums (s/spec (s/* number?))))
+
+(fact (s/valid? ::nested [:names ["a" "b"] :nums [1 2 3]]) => true)
+
+;;;; ___________________________________________________________________________
+;;;; A sequence of strings
+
+(s/def ::string string?)
+(s/def ::strings-v1 (s/* ::string))
+(s/def ::strings-v2 (s/spec (s/* ::string)))
+(s/def ::strings-v3 (s/* (s/spec ::string)))
+
+(fact (s/valid? ::string "str") => true)
+
+(fact (s/valid? ::strings-v1 ["str-1" "str-2" "str-3"]) => true)
+
+(fact (s/valid? ::strings-v2 ["str-1" "str-2" "str-3"]) => true)
+
+(fact (s/valid? ::strings-v3 ["str-1" "str-2" "str-3"]) => true)
+
+;;;; ___________________________________________________________________________
+;;;; A sequence of pairs
+;;;; - Note how  things are different to a sequence of strings. This seems
+;;;;   almost perverse to me -- it seems inconsistent with the `string?`
+;;;;   examples above.
+
+(s/def ::string-and-number (s/cat :first-item string?
+                                  :second-item number?))
+(s/def ::strings-and-numbers-v1 (s/* ::string-and-number))
+(s/def ::strings-and-numbers-v2 (s/spec (s/* ::string-and-number)))
+(s/def ::strings-and-numbers-v3 (s/* (s/spec ::string-and-number)))
+
+(fact (s/valid? ::string-and-number      ["str-1" 1])               => true)
+
+(fact (s/valid? ::strings-and-numbers-v1 ["str-1" 1])               => true)
+(fact (s/valid? ::strings-and-numbers-v1 ["str-1" 1 "str-2" 2])     => true)
+
+(fact (s/valid? ::strings-and-numbers-v2 ["str-1" 1])               => true)
+(fact (s/valid? ::strings-and-numbers-v2 ["str-1" 1 "str-2" 2])     => true)
+
+(fact (s/valid? ::strings-and-numbers-v3 [["str-1" 1] ["str-2" 2]]) => true)

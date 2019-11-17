ecfd5140a8ed169ba992d122fea5d6f654995d98
Simon Katz <sk@nomistech.com>
2019-11-17 13:18:38 +0000

    Add example about `do` and top-level forms

---
+++ src/com/nomistech/clojure_the_language/example_for_top_level_forms.clj
@@ -0,0 +1,5 @@
+(ns com.nomistech.clojure-the-language.example-for-top-level-forms)
+
+;;;; This ns is required by demo code in `com.nomistech.clojure-the-language.c-300-language-lawyer-stuff.s-100-top-level-forms`
+
+(def x 42)
---
+++ test/com/nomistech/clojure_the_language/c_300_language_lawyer_stuff/s_100_top_level_forms.clj
@@ -0,0 +1,25 @@
+(ns com.nomistech.clojure-the-language.c-300-language-lawyer-stuff.s-100-top-level-forms
+  (:require [midje.sweet :refer :all]))
+
+#_
+(do
+  ;; Demo that subforms of `do` are treated as separate compilation units
+  ;; (ie as top-level forms).
+  (remove-ns 'com.nomistech.clojure-the-language.example-for-top-level-forms)
+  (require 'com.nomistech.clojure-the-language.example-for-top-level-forms
+           :reload)
+  ;; This is OK because it is compiled after the required ns is loaded.
+  com.nomistech.clojure-the-language.example-for-top-level-forms/x)
+;; => 42
+
+#_
+(do
+  ;; Demo that `let` subforms are compiled as a single unit.
+  (remove-ns 'com.nomistech.clojure-the-language.example-for-top-level-forms)
+  (let []
+    (require 'com.nomistech.clojure-the-language.example-for-top-level-forms
+             :reload)
+    ;; This is not OK because is compiled before the required ns is loaded.
+    com.nomistech.clojure-the-language.example-for-top-level-forms/x))
+;; =throws=> java.lang.ClassNotFoundException
+;;           because ns not found when compiling .../x

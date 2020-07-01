a61c960f09897c4a74236c92f96078a2deeac8c0
Simon Katz <sk@nomistech.com>
2020-07-01 11:32:08 +0100

    Add call of `tnr/set-refresh-dirs`

--- dev/user.clj
+++ dev/user.clj
@@ -3,7 +3,8 @@
   (:require [clojure.main]
             [clojure.string :as str]
             [clojure.tools.namespace.move :refer :all]
-            [clojure.tools.namespace.repl :refer :all]
+            [clojure.tools.namespace.repl :as tnr
+             :refer [refresh refresh-all]]
             [midje.repl :refer :all]))
 
 ;;;; ___________________________________________________________________________
@@ -13,6 +14,23 @@
 ;;;; because the requires get blatted by `tnr/refresh` and `reset`.
 
 (apply require clojure.main/repl-requires)
+
+;;;; ___________________________________________________________________________
+
+;;;; `clojure.tools.namespace.repl/refresh` sometimes doesn't work, because it
+;;;; doesn't know what directories to read from when refreshing. This is a
+;;;; problem caused by different behaviour around classpaths on different
+;;;; versions of the JVM.
+;;;;
+;;;; See:
+;;;; - https://ask.clojure.org/index.php/8288/java-11-and-tools-namespace
+;;;; - https://github.com/clojure-emacs/cider/issues/2686
+;;;; - https://github.com/clojure-emacs/cider/issues/2686#issuecomment-532389499
+;;;;
+;;;; Conclusion:
+;;;; - Do the following:
+
+(tnr/set-refresh-dirs "dev" "src" "test")
 
 ;;;; ___________________________________________________________________________
 ;;;; App-specific additional utilities for the REPL or command line

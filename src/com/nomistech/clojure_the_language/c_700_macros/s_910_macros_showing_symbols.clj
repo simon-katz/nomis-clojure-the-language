3f422e6a521ab8956732163c8fd6611eed368620
Simon Katz <sk@nomistech.com>
2020-08-08 15:52:23 +0100

    Improve macro expansion demo

--- src/com/nomistech/clojure_the_language/c_700_macros/s_910_macros_showing_symbols.clj
+++ src/com/nomistech/clojure_the_language/c_700_macros/s_910_macros_showing_symbols.clj
@@ -2,16 +2,23 @@
 
 ;;;; ___________________________________________________________________________
 
-(defmacro do-things-with-symbols-1 [x]
-  `{:unquote        ~x
-    :quote-unquote '~x
-    :force-capture ~'x
-    :undecorated     x
-    :quote          'x})
+(defmacro demo-of-unqualified-symbols-in-macros []
+  `{:no-decoration--qualified-name-in-macro   name-in-macro
+    :quote--quoted-qualified-name-in-macro    'name-in-macro
+    :unquote-quote--unqualified-name-in-macro ~'name-in-macro})
 
-(defmacro do-things-with-symbols-2 []
-  `{:undecorated-value-symbol   *print-level*
-    :undecorated-fun-symbol     +
-    :undecorated-macro-symbol   cond
-    :undecorated-special-symbol if ; different -- is this for things that satisfy `special-symbol?`?
+(defmacro demo-of-qualified-symbols-in-macros []
+  `{:no-decoration-qualified--name-in-macro some-ns/name-in-macro
+    :quote-qualified--quoted-name-in-macro  'some-ns/name-in-macro
+    :unquote-quote-qualified--name-in-macro ~'some-ns/name-in-macro})
+
+(defmacro demo-of-special-symbols-in-macros []
+  `{:no-decoration-unqualified-value-symbol--qualified     *print-level*
+    :no-decoration-unqualified-fun-symbol--qualified       +
+    :no-decoration-unqualified-macro-symbol--qualified     cond
+    :no-decoration-unqualified-special-symbol--unqualified if ; different -- is this for things that satisfy `special-symbol?`?
     })
+
+(defmacro demo-of-symbols-passed-to-macros [name-in-macro]
+  `{:unquote--symbol-in-call              ~name-in-macro
+    :quote-unquote--quoted-symbol-in-call '~name-in-macro})
--- test/com/nomistech/clojure_the_language/c_700_macros/s_910_macros_showing_symbols_test.clj
+++ test/com/nomistech/clojure_the_language/c_700_macros/s_910_macros_showing_symbols_test.clj
@@ -1,20 +1,40 @@
 (ns com.nomistech.clojure-the-language.c-700-macros.s-910-macros-showing-symbols-test
   (:require [com.nomistech.clojure-the-language.c-700-macros.s-910-macros-showing-symbols :as syms]
-            [midje.sweet :refer :all]))
+            [midje.sweet :refer [fact]]))
 
 ;;;; ___________________________________________________________________________
 
-(fact
-  (macroexpand-1 '(syms/do-things-with-symbols-1 x-in-call))
-  => '{:unquote        x-in-call
-       :quote-unquote 'x-in-call
-       :force-capture  x
-       :undecorated    com.nomistech.clojure-the-language.c-700-macros.s-910-macros-showing-symbols/x
-       :quote         'com.nomistech.clojure-the-language.c-700-macros.s-910-macros-showing-symbols/x})
+(fact "About unqualified symbols in macros"
+  (macroexpand-1 '(syms/demo-of-unqualified-symbols-in-macros))
+  => '{:no-decoration--qualified-name-in-macro
+       com.nomistech.clojure-the-language.c-700-macros.s-910-macros-showing-symbols/name-in-macro
+       ;;
+       :quote--quoted-qualified-name-in-macro
+       'com.nomistech.clojure-the-language.c-700-macros.s-910-macros-showing-symbols/name-in-macro
+       ;;
+       :unquote-quote--unqualified-name-in-macro name-in-macro})
 
-(fact
-  (macroexpand-1 '(syms/do-things-with-symbols-2))
-  => '{:undecorated-value-symbol   clojure.core/*print-level*
-       :undecorated-fun-symbol     clojure.core/+
-       :undecorated-macro-symbol   clojure.core/cond
-       :undecorated-special-symbol if})
+(fact "About qualified symbols in macros"
+  (macroexpand-1 '(syms/demo-of-qualified-symbols-in-macros))
+  => '{:no-decoration-qualified--name-in-macro some-ns/name-in-macro
+       :quote-qualified--quoted-name-in-macro  'some-ns/name-in-macro
+       :unquote-quote-qualified--name-in-macro some-ns/name-in-macro})
+
+(fact "About special symbols in macros"
+  (macroexpand-1 '(syms/demo-of-special-symbols-in-macros))
+  => '{:no-decoration-unqualified-value-symbol--qualified     clojure.core/*print-level*
+       :no-decoration-unqualified-fun-symbol--qualified       clojure.core/+
+       :no-decoration-unqualified-macro-symbol--qualified     clojure.core/cond
+       :no-decoration-unqualified-special-symbol--unqualified if})
+
+(fact "About passing a symbol to a macro"
+
+  (fact "Unqualified symbol"
+    (macroexpand-1 '(syms/demo-of-symbols-passed-to-macros name-in-call))
+    => '{:unquote--symbol-in-call              name-in-call
+         :quote-unquote--quoted-symbol-in-call 'name-in-call})
+
+  (fact "Qualified symbol"
+    (macroexpand-1 '(syms/demo-of-symbols-passed-to-macros some-ns/name-in-call))
+    => '{:unquote--symbol-in-call              some-ns/name-in-call
+         :quote-unquote--quoted-symbol-in-call 'some-ns/name-in-call}))

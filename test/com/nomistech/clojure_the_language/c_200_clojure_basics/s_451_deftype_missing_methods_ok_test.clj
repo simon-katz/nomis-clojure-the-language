82f1ac52d1c22c5f205b146814575c32ad042db4
Simon Katz <sk@nomistech.com>
2021-06-22 11:52:33 +0100

    Add `s-451-deftype-missing-methods-ok-test`

---
+++ test/com/nomistech/clojure_the_language/c_200_clojure_basics/s_451_deftype_missing_methods_ok_test.clj
@@ -0,0 +1,23 @@
+(ns com.nomistech.clojure-the-language.c-200-clojure-basics.s-451-deftype-missing-methods-ok-test
+  (:require
+   [midje.sweet :refer :all]))
+
+;;;; The doc for `deftype` says "Methods should be supplied for all methods of
+;;;; the desired protocol(s) and interface(s), but that's not enforced as shown
+;;;; here.
+
+(defprotocol MyProtocol
+  (method-1 [_])
+  (method-2 [_]))
+
+(deftype MyType [x]
+  MyProtocol
+  (method-1
+    [_]
+    (+ x 100))
+  ;; Note that we are not defining a method for `method-2`.
+  )
+
+(fact "\"Methods should be supplied for all methods\" is not enforced"
+  (method-1 (->MyType 42))
+  => 142)

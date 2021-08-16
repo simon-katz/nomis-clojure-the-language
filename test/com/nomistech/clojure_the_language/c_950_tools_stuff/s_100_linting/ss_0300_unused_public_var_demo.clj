54bbdd9a68f236223ed4647f6eff2fd9c2a5ff05
Simon Katz <sk@nomistech.com>
2021-08-16 23:28:33 +0100

    Add `ss-0300-unused-public-var-demo`

---
+++ test/com/nomistech/clojure_the_language/c_950_tools_stuff/s_100_linting/ss_0300_unused_public_var_demo.clj
@@ -0,0 +1,5 @@
+(ns com.nomistech.clojure-the-language.c-950-tools-stuff.s-100-linting.ss-0300-unused-public-var-demo)
+
+(def my-unused-public-var)
+
+(def my-unused-public-var-with-config-saying-ok)

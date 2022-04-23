2813b4d415e3f51b9939a90785cff4e9dcef634d
Simon Katz <sk@nomistech.com>
2022-04-23 19:45:33 +0100

    matcher-combinators: Add examples from documentation

--- test/com/nomistech/clojure_the_language/c_800_libraries/s_106_matcher_combinators/matcher_generators_test.clj
+++ test/com/nomistech/clojure_the_language/c_800_libraries/s_106_matcher_combinators/matcher_generators_test.clj
@@ -2,7 +2,8 @@
   (:require
    [clojure.test :refer [deftest is testing]]
    [matcher-combinators.matchers :as m]
-   [matcher-combinators.test]))
+   [matcher-combinators.test] ; adds support for `match?` and `thrown-match?` in `is` expressions
+   ))
 
 ;;;; ++use-of-mismatch++
 ;;;; We use `m/mismatch` to demo what would otherwise be failing tests.
@@ -72,6 +73,80 @@
               {:a 1 :b 1234})))
 
 (deftest exception-matching-test
+  (is (thrown-match? clojure.lang.ExceptionInfo
+                     {:foo 1}
+                     (throw (ex-info "Boom!" {:foo 1 :bar 2})))))
+
+;;;; ___________________________________________________________________________
+
+;;;; From docs
+
+(deftest test-matching-with-explicit-matchers
+  (is (match? (m/equals 37) (+ 29 8)))
+  (is (match? (m/regex #"fox") "The quick brown fox jumps over the lazy dog")))
+
+(deftest test-matching-scalars
+  ;; most scalar values are interpreted as an `equals` matcher
+  (is (match? 37 (+ 29 8)))
+  (is (match? "this string" (str "this" " " "string")))
+  (is (match? :this/keyword (keyword "this" "keyword")))
+  ;; regular expressions are handled specially
+  (is (match? #"fox" "The quick brown fox jumps over the lazy dog")))
+
+(deftest test-matching-sequences
+  ;; A sequence is interpreted as an `equals` matcher, which specifies
+  ;; count and order of matching elements. The elements, themselves,
+  ;; are matched based on their types.
+  (is (match? [1 3] [1 3]))
+  (is (match? [1 odd?] [1 3]))
+  (is (match? [#"red" #"violet"] ["Roses are red" "Violets are ... violet"]))
+
+  ;; use m/prefix when you only care about the first n items
+  (is (match? (m/prefix [odd? 3]) [1 3 5]))
+
+  ;; use m/in-any-order when order doesn't matter
+  (is (match? (m/in-any-order [odd? odd? even?]) [1 2 3]))
+
+  ;; NOTE: in-any-order is O(n!) because it compares every expected element
+  ;; with every actual element in order to find a best-match for each one,
+  ;; removing matched elements from both sequences as it goes.
+  ;; Avoid applying this to long sequences.
+  )
+
+(deftest test-matching-sets
+  ;; A set is also interpreted as an `equals` matcher.
+  (is (match? #{1 2 3} #{3 2 1}))
+  (is (match? #{odd? even?} #{1 2}))
+  ;; use m/set-equals to repeat predicates
+  (is (match? (m/set-equals [odd? odd? even?]) #{1 2 3}))
+
+  ;; NOTE: matching sets is an O(n!) operation because it compares every
+  ;; expected element with every actual element in order to find a best-match
+  ;; for each one, removing matched elements from both sets as it goes.
+  ;; Avoid applying this to large sets.
+  )
+
+(deftest test-matching-maps
+  ;; A map is interpreted as an `embeds` matcher, which ignores
+  ;; un-specified keys
+  (is (match? {:name/first "Alfredo"}
+              {:name/first  "Alfredo"
+               :name/last   "da Rocha Viana"
+               :name/suffix "Jr."})))
+
+(deftest test-matching-nested-datastructures
+  ;; Maps, sequences, and sets follow the same semantics whether at
+  ;; the top level or nested within a structure.
+  (is (match? {:band/members [{:name/first "Alfredo"}
+                              {:name/first "Benedito"}]}
+              {:band/members [{:name/first  "Alfredo"
+                               :name/last   "da Rocha Viana"
+                               :name/suffix "Jr."}
+                              {:name/first "Benedito"
+                               :name/last  "Lacerda"}]
+               :band/recordings []})))
+
+(deftest exception-matching
   (is (thrown-match? clojure.lang.ExceptionInfo
                      {:foo 1}
                      (throw (ex-info "Boom!" {:foo 1 :bar 2})))))

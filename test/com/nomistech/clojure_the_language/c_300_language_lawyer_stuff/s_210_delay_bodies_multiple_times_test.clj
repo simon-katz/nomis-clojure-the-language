7d1285c0378d1df290be69846e5bffa77640c533
Simon Katz <sk@nomistech.com>
2023-06-03 10:38:39 +0100

    Add demo of deadlock

--- test/com/nomistech/clojure_the_language/c_300_language_lawyer_stuff/s_210_delay_bodies_multiple_times_test.clj
+++ test/com/nomistech/clojure_the_language/c_300_language_lawyer_stuff/s_210_delay_bodies_multiple_times_test.clj
@@ -1,4 +1,5 @@
-(ns com.nomistech.clojure-the-language.c-300-language-lawyer-stuff.s-210-delay-bodies-multiple-times-test)
+(ns com.nomistech.clojure-the-language.c-300-language-lawyer-stuff.s-210-delay-bodies-multiple-times-test
+  (:require [com.climate.claypoole :as cp]))
 
 ;;;; ___________________________________________________________________________
 ;;;; self-referential-delays
@@ -52,3 +53,54 @@
 (comment
   ;; See comment above -- the same happens here.
   (self-referential-single-delay))
+
+;;;; ___________________________________________________________________________
+;;;; Demo of deadlock
+
+(def n-delays 10)
+
+(def base-map (let [pairs (map (fn [i] [i
+                                        (if (= i (dec n-delays))
+                                          0
+                                          (inc i))])
+                               (range n-delays))]
+                (apply sorted-map (apply concat pairs))))
+
+(defn base-map-key? [x] (contains? (set (keys base-map)) x))
+
+(def n-oustanding-delays& (atom 0))
+
+(defn demo-deadlock []
+  (let [key->delay& (atom base-map)]
+    (swap! key->delay&
+           update-vals
+           (fn [v]
+             (println "Creating delay for" v)
+             (delay
+               (println "In delay body for" v)
+               (swap! n-oustanding-delays& inc)
+               (assert (base-map-key? v) (format "v = %s" v))
+               (let [other-delay (get @key->delay& v)]
+                 (assert (delay? other-delay))
+                 (let [res (force other-delay)]
+                   (println "Returning from delay for" v "-- value =" res)
+                   (swap! n-oustanding-delays& dec)
+                   res)))))
+    (cp/pmap (count base-map)
+             (fn [k] (let [key->delay @key->delay&
+                           d          (get key->delay k)]
+                       (force d)))
+             (keys base-map))[]))
+
+(comment
+  ;; Just evaluating the following returns `[] 10`. Why `[]`?
+  ;;
+  ;; If you first compile `demo-deadlock` in CIDER's debug mode, then
+  ;; evaluate the following, and then step through, the forcing will lead to:
+  ;; - Many "In delay body..." messages.
+  ;; - Eventually deadlock, waiting forever for a delay. If you break, you will
+  ;;   see that the top of the stack is
+  ;;   `java.util.concurrent.FutureTask/awaitDone`.
+  [(demo-deadlock)
+   (do (Thread/sleep 100)
+       @n-oustanding-delays&)])
